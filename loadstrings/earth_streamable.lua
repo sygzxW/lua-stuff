aa6b7619155a6830a73d5560ea9878c3bcb12547
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-09-01 20:32:18 +0200

    Update earth_streamable.lua

--- loadstrings/earth_streamable.lua
+++ loadstrings/earth_streamable.lua
@@ -1,1 +1,7 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sygzxW/lua-stuff/main/scripts/earth_streamable.lua"))()
+DaHoodSettings.Prediction = 0.1344
+Aiming.TargetPart = {"Head", "UpperTorso", "LowerTorso", "HumanoidRootPart", "RightFoot", "LeftFoot"}
+Aiming.FOV = 16
+Aiming.FOVSides = 25
+Aiming.HitChance = 110
+Aiming.ShowFOV = false
