095102ffc302f7b45e9c5620146d04912aecd985
sygzxW <73584462+sygzxW@users.noreply.github.com>
2023-04-21 19:53:22 +0200

    Update kudo_gui_wl.lua

--- api/kudo_gui_wl.lua
+++ api/kudo_gui_wl.lua
@@ -1,6 +1,9 @@
-wlted = {
+return = {
     2743908389, -- raymy alt
-    532976710, -- me (sygzx)
+    532976710, -- sygzx
     722529705, -- sivyen(kudo)
+	697179005, -- gauthy
+	3870466648, -- komendziak
+	3637717891, -- helloooooo alt
+	192640445, -- hard
 }
-return wlted
